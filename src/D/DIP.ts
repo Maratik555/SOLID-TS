4605be2392a501d781ece3b5de459dba8c45c127
Маратик555 <amr404@bk.ru>
2022-05-03 11:38:22 +0300

    ok

---
+++ src/D/DIP.ts
@@ -0,0 +1,41 @@
+
+interface MusicApi {
+  getTracks: () => void;
+}
+
+class YandexMusicApi implements MusicApi {
+  getTracks(): void {}
+}
+
+class SpotifyApi implements MusicApi {
+  getTracks(): void {}
+}
+
+class VKMusicApi implements MusicApi {
+  getTracks(): void {}
+}
+
+class MusicClient implements MusicApi {
+  client: MusicApi;
+
+  constructor(client: MusicApi) {
+    this.client = client;
+  }
+
+  getTracks() {
+    this.client.getTracks();
+  }
+}
+
+const MusicApp = () => {
+  const API = new MusicClient(new SpotifyApi())
+
+  API.getTracks()
+}
+
+
+interface HttpClient {
+
+}
+
+
